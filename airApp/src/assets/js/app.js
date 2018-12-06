6e44468b851ad7b5b6a2b6746edf49a02daed8dc
seancote <sccote@gmail.com>
2018-12-06 18:22:36 -0500

    all the ionic work so far

---
+++ airApp/src/assets/js/app.js
@@ -0,0 +1,14 @@
+function onBtnDownClicked (){
+  window.location="#page2";
+}
+
+
+function move_down (){
+	console.log("test");
+}
+
+var component = HaikuComponentEmbed_vill4f4n_AirQualiD2(
+  document.getElementById('mount'),
+  {loop: false}
+);
+var timeline = component.getDefaultTimeline();
--- airApp/src/pages/about/about.ts
+++ airApp/src/pages/about/about.ts
@@ -1,37 +1,15 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  pet: string = "puppies";
+  isAndroid: boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
-  }
-
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(AboutPage, {
-      item: item
-    });
+  constructor(platform: Platform) {
+    this.isAndroid = platform.is('android');
   }
 }
